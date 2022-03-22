66e7127d1f10922de317bbcad3ad53af14e4e619
jmmvgaa <joaomarcosvga@gmail.com>
2022-03-22 20:21:48 -0300

    first commit

--- app/src/main/java/com/example/appcalculodeimc/MainActivity.java
+++ app/src/main/java/com/example/appcalculodeimc/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.appcalculodeimc;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,10 +10,10 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView txttitulo;
-    EditText massa;
-    EditText altura;
-    TextView txtmassa;
-    TextView txtaltura;
+    EditText gasolina;
+    EditText alcool;
+    TextView txtgasolina;
+    TextView txtalcool;
     Button botao;
     TextView txtcalculo;
     TextView um;
@@ -37,10 +36,10 @@ public class MainActivity extends AppCompatActivity {
     protected void loadComponents(){
 
         txttitulo = findViewById(R.id.txttitulo);
-        massa = findViewById(R.id.massa);
-        altura = findViewById(R.id.altura);
-        txtmassa = findViewById(R.id.txtmassa);
-        txtaltura = findViewById(R.id.txtaltura);
+        gasolina = findViewById(R.id.gasolina);
+        alcool = findViewById(R.id.alcool);
+        txtgasolina = findViewById(R.id.txtgasolina);
+        txtalcool = findViewById(R.id.txtalcool);
         botao = findViewById(R.id.botao);
         txtcalculo = findViewById(R.id.txtcalculo);
 
@@ -48,21 +47,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
     protected void calculoimc(){
-        double massa1 = Double.parseDouble(massa.getText().toString());
-        double altura1 = Double.parseDouble(altura.getText().toString());
+        double massa1 = Double.parseDouble(gasolina.getText().toString());
+        double altura1 = Double.parseDouble(alcool.getText().toString());
 
-        double txtcalculo = massa1 / (altura1 * altura1);
+        double txtcalculo = massa1 * 0.7;
+        if (altura1 <= txtcalculo) dois.setText("Alcool é melhor");
+        else dois.setText("Gasolina é melhor");
 
         Log.d("resultado", String.valueOf(txtcalculo));
 
-        if(txtcalculo < 16) dois.setText("Magreza grave. "+ " " + "Seu imc é: " + txtcalculo);
-        else if(txtcalculo > 16 && txtcalculo < 17) dois.setText("Magreza moderada. "+ " " + "Seu imc é: " + txtcalculo);
-        else if(txtcalculo > 17 && txtcalculo < 18.5) dois.setText("Magreza leve. "+ " " + "Seu imc é: " + txtcalculo);
-        else if(txtcalculo > 18.5 && txtcalculo < 25) dois.setText("Saudável. "+ " " + "Seu imc é: " + txtcalculo);
-        else if(txtcalculo > 25 && txtcalculo < 30) dois.setText("Sobrepeso. "+ " " + "Seu imc é: " + txtcalculo);
-        else if(txtcalculo > 30 && txtcalculo < 35) dois.setText("Obesidade grau I. "+ " " + "Seu imc é: " + txtcalculo);
-        else if(txtcalculo > 35 && txtcalculo < 40) dois.setText("Obesidade grau II. "+ " " + "Seu imc é: " + txtcalculo);
-        else if(txtcalculo >= 40) dois.setText("Obesidade grau III (Mórbida). "+ " " + "Seu imc é: " + txtcalculo);
     }
 
     protected void botaoconfig(){
